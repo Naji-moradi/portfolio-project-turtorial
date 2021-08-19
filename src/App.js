f4bc6da1472f0c4dd3e106b1d5dafe3d48045999
akhtar mohammad <akhtarmoradi303@gmail.com>
2021-08-20 02:23:45 +0530

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
-
+import Navbar from './component/Navbar';
+ 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h2>portfolio project...</h2>
     
     </div>
